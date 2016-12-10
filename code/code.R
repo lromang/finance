febf06ababb2ce8ca4c126520fb532bbedfe3826
lromang <luis.roangarci@gmail.com>
2016-12-10 15:04:09 -0600

    dates

--- code/code.R
+++ code/code.R
@@ -11,13 +11,31 @@ library(plyr)
 library(dplyr)
 library(data.table)
 library(ggplot2)
+library(lubridate)
+library(stringr)
 
 ## ------------------------------
-## Read in datasets:
+## Read in datasets
 ## ------------------------------
 data.comp <- fread("../data/compustatFA.csv")
+names(data.comp) <- tolower(names(data.comp))
 data.crsp <- fread("../data/CRSP.csv")
+names(data.crsp) <- tolower(names(data.crsp))
 
 ## ------------------------------
-## Calculate variables:
+## Process datasets
 ## ------------------------------
+
+## Dates crsp
+year <-
+
+
+## ------------------------------
+## Calculate variables
+## ------------------------------
+
+## Enterprise number
+enterprises   <- data.crsp$comnam
+n_enterprises <- length(unique(data.crsp$comnam))
+##
+n_enterprise <- data.crsp[,.N, by = comnam]
